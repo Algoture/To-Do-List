ff828a8ed1c678d22e5ce50a3552d1890b574d59
Umesh Nagare <umeshnagare27@gmail.com>
2024-01-26 23:46:48 +0530

    Done

---
+++ script.js
@@ -0,0 +1,34 @@
+let inBox = document.getElementById("input-box");
+let listContainer = document.getElementById("list-container");
+function addTask() {
+    if (inBox.value === '') {
+        alert("Add something !");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inBox.value = '';
+    saveData();
+}
+listContainer.addEventListener("click", (e) => {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showData(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showData();
